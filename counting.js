83909b3b053443a5fa1c1cf45c17f0590c04bfca
han20001020 <408631082@o365.tku.edu.tw>
2024-11-20 16:06:07 +0800

    1

--- counting.js
+++ counting.js
@@ -10,17 +10,17 @@ const collectionName = "studentslist";
     const client = new MongoClient(uri);
 
     try {
-        // Connect to MongoDB
+       
         await client.connect();
         console.log("成功連接到 MongoDB");
 
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
 
-        // Query all student data
+    
         const students = await collection.find().toArray();
 
-        // Calculate department count
+      
         const departmentCount = students.reduce((acc, student) => {
             const 院系 = student.院系; // 假設學生資料中有 `department` 欄位
             if (院系) {
@@ -29,7 +29,7 @@ const collectionName = "studentslist";
             return acc;
         }, {});
 
-        // Display results
+    
         console.log("科系人數統計：");
         Object.entries(departmentCount).forEach(([院系, count]) => {
             console.log(`科系：${院系}, 人數：${count}`);
